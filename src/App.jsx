e95c70b2a138cab77baf2e541a83fd7ab4d598b2
Augusto Abel <uni.augusto.abel@gmail.com>
2025-06-23 15:41:16 -0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
+// Renomeando 'BrowserRouter' para 'Router' na importação
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ResultsTable from './pages/DistanceBtwCustomerAndSeller/ResultsTable.jsx'; // ajuste o caminho conforme necessário
 import Home from './pages/Home.jsx'; // exemplo de outra página
 import MapView from "./pages/DistanceBtwCustomerAndSeller/MapView.jsx";
 
-
-
 function App() {
   const [jsonData, setJsonData] = useState(null);
   return (
-    <Router>
+    // ADICIONE A PROPRIEDADE 'basename' AQUI
+    <Router basename="/pages-vbn">
       <Routes>
         <Route path="/" element={<Home setJsonData={setJsonData} />} />
         <Route path="/results" element={<ResultsTable data={jsonData} />} />
